013e61b6744d69f5b797a81049e2ef49c0f57a20
Alexander Book <Alexander.Book@frm2.tum.de>
2023-01-19 17:01:05 +0100

    version bump

--- skipi/__init__.py
+++ skipi/__init__.py
@@ -2,4 +2,4 @@ from skipi.domain import Domain, Domain2D
 from skipi.function import Function, FunctionFileLoader, ComplexFunction, PiecewiseFunction, StitchedFunction
 from skipi.fourier import FourierTransform, InverseFourierTransform
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
